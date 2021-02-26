d3c5390739ecfd0be2fdfabb6cf07c48c3b244ba
dziaro44 <56294493+dziaro44@users.noreply.github.com>
2021-02-26 04:57:18 +0100

    Create test.sh

---
+++ pipe/test.sh
@@ -0,0 +1,11 @@
+tests=`sed '1q;d' 1.in`
+for i in $(seq 2 $(($tests+1)));
+do
+	input=`sed "$i q;d" 1.in`
+	result=`./$1 $input`
+	com=`sed "$i q;d" test1.in`
+	exp_result=`bash -c "$com"` 
+	#echo "res: " $result ", expres: "  $exp_result
+	if [[ "$result" == "$exp_result" ]]; then : ; else echo 0; exit 0; fi;
+done
+echo 1
